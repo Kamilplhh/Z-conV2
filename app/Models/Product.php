026c921f9d5695f83778a75a299baca6948a1952
Kamil <kamilczekotas@gmail.com>
2024-02-03 20:58:18 +0100

    feat: product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'producer',
+        'price',
+        'promo',
+        'stock',
+        'spec_id'
+    ];
 }
