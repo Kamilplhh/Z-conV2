3248b67e5c947bca52a7198ab49456298146e7f2
Kamil <kamilczekotas@gmail.com>
2024-02-03 20:57:35 +0100

    feat: product model and migration

---
+++ database/migrations/2024_02_03_195109_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('photo')->default('noPhoto.jpg');
+            $table->string('producer');
+            $table->float('price', 8, 2);
+            $table->integer('promo')->default(0);
+            $table->integer('stock')->default(0);
+            $table->string('spec_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
