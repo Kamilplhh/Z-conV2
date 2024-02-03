ae8e09a91ffde369269110258a022d230faabed5
Kamil <kamilczekotas@gmail.com>
2024-02-03 21:01:29 +0100

    feat: orderedProducts model and migration

---
+++ database/migrations/2024_02_03_195851_create_ordered_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ordered_products', function (Blueprint $table) {
+            $table->id();
+            $table->float('price', 8, 2);
+            $table->integer('quantity');
+            $table->bigInteger('product_id');
+            $table->bigInteger('order_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ordered_products');
+    }
+};
