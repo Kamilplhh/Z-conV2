f91adcf6078812999c3df16dcf38403edb4c90d9
Kamil <kamilczekotas@gmail.com>
2024-02-03 21:22:45 +0100

    feat: Categories model and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'label_1',
+        'label_2',
+        'label_3',
+        'label_4',
+        'label_5',
+        'label_6',
+        'label_7',
+        'label_8',
+        'label_9',
+        'label_10',
+        'label_11',
+        'label_12',
+    ];
+}
