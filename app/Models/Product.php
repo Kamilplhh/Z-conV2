96f9110b6d248a7737f00e903f5342a82fce1059
Kamil <kamilczekotas@gmail.com>
2024-02-04 22:09:31 +0100

    feat: Product model relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Spec;
 
 class Product extends Model
 {
@@ -18,4 +19,9 @@ class Product extends Model
         'stock',
         'spec_id'
     ];
+
+    public function Spec()
+    {
+        return $this->belongsTo(Spec::class);
+    }
 }
