fe5f9f772e52ad206bcd43a4b77330063594de9f
Kamil <kamilczekotas@gmail.com>
2024-02-04 22:10:38 +0100

    feat: Review model relationship

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+use App\Models\USer;
 
 class Review extends Model
 {
@@ -14,4 +16,14 @@ class Review extends Model
         'product_id',
         'user_id'
     ];
+
+    public function Product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
